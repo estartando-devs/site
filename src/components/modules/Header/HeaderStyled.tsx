179187208528491a08fc0c2baa4a69def92a78a4
Breno Nunes <brenontavares@gmail.com>
2020-06-27 22:59:00 -0300

    Header Module Component

---
+++ src/components/modules/Header/HeaderStyled.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import media from "../../../styles/breakpoints";
+import { backgroundHeader } from "../../../shared/img";
+
+const HeaderContainer = styled.header`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    background-image: url(${backgroundHeader});
+    background-attachment: fixed;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    ${media.greaterThan("small")`
+        height: 1024px;
+    `}
+`;
+
+const ScrollLink = styled.a`
+    position: absolute;
+    cursor: pointer;
+    bottom: 30px;
+`;
+
+const ScrollImage = styled.img`
+    width: 32px;
+    height: auto;
+`;
+
+export { HeaderContainer, ScrollLink, ScrollImage };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <>site devs</>;
+  return <div id="scrollDestiny">site devs</div>;
 };
 
 export default Home;
