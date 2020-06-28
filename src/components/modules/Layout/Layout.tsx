0a58abf0241a7359f711590d937c899d015490d2
Breno Nunes <brenontavares@gmail.com>
2020-06-27 23:03:51 -0300

    Fixing deploy error

--- src/components/modules/Layout/Layout.tsx
+++ src/components/modules/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import * as S from "./LayoutStyled";
-import { Header } from "../Header";
 
 interface LayoutProps {
   children: React.ReactNode
@@ -9,7 +8,6 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => (
   <S.LayoutContainer>
     <S.LayoutContent>
-      <Header />
       {children}
     </S.LayoutContent>
   </S.LayoutContainer>
