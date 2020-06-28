fe30fc5129497dd1022be01f71819b04d185ec94
Breno Nunes <brenontavares@gmail.com>
2020-06-28 03:26:28 -0300

    Images of slide items

---
+++ src/components/features/SectionSlide/assets/index.ts
@@ -0,0 +1,6 @@
+import slideBtn from "./slide-button.svg";
+import slideBtn2 from "./slide-button2.svg";
+import slideBtn3 from "./slide-button3.svg";
+import slideBtn4 from "./slide-button4.svg";
+
+export { slideBtn, slideBtn2, slideBtn3, slideBtn4 };
