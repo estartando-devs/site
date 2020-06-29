445d2f97c50b9b5159d61d6b03a7cd0623cc5668
Breno Nunes <brenontavares@gmail.com>
2020-06-29 04:15:30 -0300

    Add arrow back icons

--- src/components/features/SectionSlide/assets/index.ts
+++ src/components/features/SectionSlide/assets/index.ts
@@ -2,5 +2,7 @@ import slideBtn from "./slide-button.svg";
 import slideBtn2 from "./slide-button2.svg";
 import slideBtn3 from "./slide-button3.svg";
 import slideBtn4 from "./slide-button4.svg";
+import iconBack from "./icon-back.svg";
+import iconBackWhite from "./icon-back-white.svg";
 
-export { slideBtn, slideBtn2, slideBtn3, slideBtn4 };
+export { slideBtn, slideBtn2, slideBtn3, slideBtn4, iconBack, iconBackWhite };
