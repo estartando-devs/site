000fe0286f696a0c281dd32397434e5268fce260
Breno Nunes <brenontavares@gmail.com>
2020-06-29 04:18:55 -0300

    removing slides of SlideItems component

--- src/components/features/SectionSlide/SlideItems/SlideItems.tsx
+++ src/components/features/SectionSlide/SlideItems/SlideItems.tsx
@@ -1,68 +1,34 @@
 import React from "react";
-import * as S from "./SlideItemsStyled";
-import { nossaMetodologia } from "../../../../shared/img";
-import { slideBtn, slideBtn2, slideBtn3, slideBtn4 } from "../assets";
+import Slides from "./items/index";
 
-interface IItem {
-  onNext: () => void;
+export interface IItem {
+  onPrevious?: () => void;
+  onNext?: () => void;
   position?: number;
 }
 
-const FirstItem = ({ onNext, position }: IItem) => (
-  <S.SectionContainer bgColor="#161616" position={position} >
-    <S.SectionTitle>Conheça o projeto e nossa metodologia</S.SectionTitle>
-    <S.SectionImage src={nossaMetodologia} alt="Nossa Metodologia" />
-    <S.ButtonSlide src={slideBtn} alt="clique e conheça" onClick={() => onNext()} />
-  </S.SectionContainer>
-);
-
-const SecondItem = ({ onNext, position }: IItem) => (
-  <S.SectionContainer bgColor="#00BFA6" position={position}>
-    <S.SectionTitle color="#3C3C3C">
-      Nosso MÉTODO
-    </S.SectionTitle>
-    <S.Text>Nossa proposta de ensino coloca o aluno como protagonista. Incentivamos a autonomia e dedicação pessoal para o avanço no curso.</S.Text>
-    <S.Text>Nossas aulas, com conteúdos práticos e bem hands-on, preparam os alunos para a realidade do mercado de trabalho.</S.Text>
-    <S.Text>Além dos conteúdos específicos de cada curso, apresentamos as principais ferramentas Open Source exigidas pelo mercado e trabalhamos no desenvolvimento das softs-skills mais desejadas pelo mercado da tecnologia:</S.Text>
-    <S.ButtonSlide src={slideBtn2} alt="clique e conheça" onClick={() => onNext()} />
-  </S.SectionContainer>
-);
-
-const ThirdItem = ({ onNext, position }: IItem) => (
-  <S.SectionContainer bgColor="#161616" position={position}>
-    <S.SectionTitle>Terceiro Slide</S.SectionTitle>
-    <S.ButtonSlide src={slideBtn3} alt="clique e conheça" onClick={() => onNext()} />
-  </S.SectionContainer>
-);
-
-const FourthItem = ({ onNext, position }: IItem) => (
-  <S.SectionContainer bgColor="#F7F7F7" position={position}>
-    <S.SectionTitle>Quarto Slide</S.SectionTitle>
-    <S.ButtonSlide src={slideBtn4} alt="clique e conheça" onClick={() => onNext()} />
-  </S.SectionContainer>
-);
-
 interface ISlideItems {
   position: number;
-  setNext: () => void;
+  onPrevious?: () => void;
+  setNext?: () => void;
 }
 
-const SlideItems = ({ position, setNext }: ISlideItems) => {
-  const items = [
-    <FirstItem onNext={setNext} />,
-    <SecondItem onNext={setNext} />,
-    <ThirdItem onNext={setNext} />,
-    <FourthItem onNext={setNext} />,
+const SlideItems = ({ position, setNext, onPrevious }: ISlideItems) => {
+  const items: JSX.Element[] = [
+    <Slides.FirstSlide onNext={setNext} />,
+    <Slides.SecondSlide onNext={setNext} />,
   ];
 
   if (position > items.length - 1) return items[items.length - 1];
 
   return (
     <>
-      <FirstItem onNext={setNext} position={position} />
-      <SecondItem onNext={setNext} position={position} />
-      <ThirdItem onNext={setNext} position={position} />
-      <FourthItem onNext={setNext} position={position} />
+      <Slides.FirstSlide onNext={setNext} position={position} />
+      <Slides.SecondSlide
+        onPrevious={onPrevious}
+        onNext={onPrevious}
+        position={position}
+      />
     </>
   );
 };
--- src/components/features/SectionSlide/SlideItems/SlideItemsStyled.tsx
+++ src/components/features/SectionSlide/SlideItems/SlideItemsStyled.tsx
@@ -1,57 +1,37 @@
 import styled from "styled-components";
-import { Typography } from "../../../elements";
 
 interface ISectionContainer {
   bgColor?: String;
 }
 
-const SectionContainer = styled.div.attrs((props: ISectionContainer) => ({
-  style: { backgroundColor: props.bgColor },
+const SlideContainer = styled.div.attrs((props: ISectionContainer) => ({
+  style: {
+    backgroundColor: props.bgColor,
+  },
 }))<ISectionContainer>`
-    min-height: 100%;
-    min-width: 100%;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: flex-start;
-    padding: 0 65px 0 20px;
-    overflow: hidden;
-    transition: .5s;
-    transform: ${(props) => `translateX(${props.position * -100}%)`};
+  min-height: 100vh;
+  min-width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  padding: 0 65px 0 20px;
+  overflow: hidden;
+  transition: 0.5s;
+  transform: ${(props) => `translateX(${props.position * -100}%)`};
 `;
 
-const SectionTitle = styled(Typography).attrs({
-  variant: "h1",
-  weight: "bold",
-})`
-  max-width: 240px;
-  line-height: 37px;
+const ButtonNext = styled.img`
+  position: absolute;
+  height: 100%;
+  right: 0;
 `;
 
-const Text = styled(Typography).attrs({
-  variant: "body1",
-  color: "#3C3C3C",
-})`
-
+const ButtonPrevious = styled.img`
+  position: absolute;
+  top: 17px;
+  left: 17px;
 `;
 
-const ButtonSlide = styled.img`
-    position: absolute;
-    height: 100%;
-    right: 0;
-`;
-
-const SectionImage = styled.img`
-    width: 100%;
-    height: auto;
-    max-width: 300px;
-`;
-
-export {
-  SectionContainer,
-  SectionTitle,
-  Text,
-  ButtonSlide,
-  SectionImage,
-};
+export { SlideContainer, ButtonNext, ButtonPrevious };
