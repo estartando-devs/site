00a93d3e754bcd327f1a791ddfddf4ce5d6a0672
Ana <anamurdock15@gmail.com>
2020-06-30 19:49:50 -0300

    Logo animation

--- src/components/elements/Logo/LogoStyled.tsx
+++ src/components/elements/Logo/LogoStyled.tsx
@@ -1,4 +1,4 @@
-import styled, { css, keyframes } from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 interface LogoProps {
     width: string,
@@ -6,20 +6,68 @@ interface LogoProps {
     padding: string,
     margin: string
     position: string,
-    animation: string,
 }
 
-const rotate = keyframes`
-    from { 
-        transform: rotate(0deg);
-    }
-    to {  
-        transform: rotate(360deg);
+const keyframeAnimation = keyframes`
+    to {
+        stroke-dashoffset: 0;
     }
 `;
 
-export const rotateRule = css`
-  ${rotate} 20s infinite linear;
+export const LogoSvg = styled.svg`
+path:nth-child(1){
+    stroke-dasharray: 2894;
+    stroke-dashoffset: 2894;
+    animation: ${keyframeAnimation} 2s ease forwards;
+}
+
+path:nth-child(2){
+    stroke-dasharray: 2221;
+    stroke-dashoffset: 2221;
+    animation: ${keyframeAnimation} 2s ease forwards 0.3s;
+}
+
+path:nth-child(3){
+    stroke-dasharray: 2458;
+    stroke-dashoffset: 2458;
+    animation: ${keyframeAnimation} 2s ease forwards 0.6s;
+}
+
+path:nth-child(4){
+    stroke-dasharray: 1849;
+    stroke-dashoffset: 1849;
+    animation: ${keyframeAnimation} 2s ease forwards 0.9s;
+}
+
+path:nth-child(5){
+    stroke-dasharray: 944;
+    stroke-dashoffset: 944;
+    animation: ${keyframeAnimation} 2s ease forwards 1.2;
+}
+
+path:nth-child(6){
+    stroke-dasharray: 1971;
+    stroke-dashoffset: 1971;
+    animation: ${keyframeAnimation} 2s ease forwards 1.5s;
+}
+
+path:nth-child(7){
+    stroke-dasharray: 2167;
+    stroke-dashoffset: 2167;
+    animation: ${keyframeAnimation} 2s ease forwards 2.8s;
+}
+
+path:nth-child(8){
+    stroke-dasharray: 2283;
+    stroke-dashoffset: 2283;
+    animation: ${keyframeAnimation} 2s ease forwards 2.1;
+}
+
+path:nth-child(9){
+    stroke-dasharray: 8354;
+    stroke-dashoffset: 8354;
+    animation: ${keyframeAnimation} 2s ease forwards 2.1;
+}
 `
 
 export const LogoContainer = styled.img<LogoProps>`
@@ -29,5 +77,4 @@ height: ${(props) => props.height};
 padding: ${(props) => props.padding};
 margin: ${(props) => props.margin};
 position: ${(props) => props.position};
-animation: ${(props) => props.animation};
 `;
