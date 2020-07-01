f5ffed5ca3f7361824abbbd5a637c4cae930ac2b
Breno Nunes <brenontavares@gmail.com>
2020-07-01 03:17:25 -0300

    get height from window

--- src/components/modules/Header/HeaderStyled.tsx
+++ src/components/modules/Header/HeaderStyled.tsx
@@ -3,32 +3,32 @@ import media from "../../../styles/breakpoints";
 import { backgroundHeader } from "../../../shared/img";
 
 const HeaderContainer = styled.header`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    background-image: url(${backgroundHeader});
-    background-attachment: fixed;
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
+  width: 100%;
+  height: ${window.innerHeight}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background-image: url(${backgroundHeader});
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 
-    ${media.greaterThan("small")`
+  ${media.greaterThan("small")`
         height: 100vh;
     `}
 `;
 
 const ScrollLink = styled.a`
-    position: absolute;
-    cursor: pointer;
-    bottom: 30px;
+  position: absolute;
+  cursor: pointer;
+  bottom: 30px;
 `;
 
 const ScrollImage = styled.img`
-    width: 32px;
-    height: auto;
+  width: 32px;
+  height: auto;
 `;
 
 export { HeaderContainer, ScrollLink, ScrollImage };
