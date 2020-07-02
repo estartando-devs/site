ca929ba219c2bc6a66f4338b99d4a59f0fb8615d
Breno Nunes <brenontavares@gmail.com>
2020-07-02 01:42:31 -0300

    OurMethod Responsive

--- src/components/features/SectionSlide/SlideItems/items/OurMethod/OurMethodStyled.tsx
+++ src/components/features/SectionSlide/SlideItems/items/OurMethod/OurMethodStyled.tsx
@@ -14,18 +14,25 @@ interface IOMTagsBox {
 
 const OMContainer = styled(SlideContainer)`
   justify-content: flex-start;
-  padding: 0 90px 0 45px;
+  align-items: flex-start;
+  height: 100%;
+  padding: 0;
 
   ${media.lessThan("medium")`
     background-image: url(${waves});
     background-repeat: no-repeat;
     background-position: bottom;
     background-size: contain;
-  `}
+  `};
 `;
 
 const OMContent = styled.div`
   margin: 50px 0;
+  padding: 0 90px 0 45px;
+
+  ${media.greaterThan("medium")`
+    padding: 64px 264px;
+  `}
 `;
 
 const OMTitle = styled(Typography).attrs({
@@ -60,6 +67,7 @@ const OMTagsBox = styled.div<IOMTagsBox>`
 const OMTag = styled(OMText)`
   margin: 5px 10px;
   font-weight: 600;
+  color: #3c3c3c;
 
   ${media.lessThan("medium")`
     color: #00bfa6;
