6a212866ae3a1ab489a55ae21bdff04a001e8586
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-07-03 02:00:25 -0300

    chore: include supporters buttons redirect

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import * as S from "./FooterStyled";
 import { SocialShareButtons } from "../SocialShareButtons";
 import { Logo } from "../../elements";
+import { Supporters } from "../Supporters";
 
 const Footer = () => (
   <S.FooterContainer>
+    <Supporters />
     <Logo />
     <SocialShareButtons />
   </S.FooterContainer>
