aac8d21e2af955965e0980a990f46c815335b316
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-07-02 21:56:32 -0300

    fix: responsive status for AnimatedLogo

--- src/components/elements/AnimatedLogo/AnimatedLogoStyled.tsx
+++ src/components/elements/AnimatedLogo/AnimatedLogoStyled.tsx
@@ -1,78 +1,79 @@
 import styled, { keyframes } from "styled-components";
-
-interface AnimatedLogoProps {
-    width: string,
-    height: string,
-    padding: string,
-    margin: string
-}
+import media from "styled-media-query";
 
 const keyframeAnimation = keyframes`
-    to {
-        stroke-dashoffset: 0;
-    }
+  to {
+    stroke-dashoffset: 0;
+  }
 `;
 
 export const AnimatedLogoSvg = styled.svg`
-path:nth-child(1){
+  ${media.lessThan("medium")`
+    width: 440px;
+    height: auto;
+  `}
+
+  ${media.lessThan("small")`
+    width: 280px;
+    height: auto;
+  `}
+
+  path {
+    stroke: white;
+    stroke-width: 3;
+  }
+
+  path:nth-child(1){
     stroke-dasharray: 545;
     stroke-dashoffset: 545;
     animation: ${keyframeAnimation} 2s ease forwards;
-}
+  }
 
-path:nth-child(2){
+  path:nth-child(2){
     stroke-dasharray: 420;
     stroke-dashoffset: 420;
     animation: ${keyframeAnimation} 2s ease forwards 0.3s;
-}
+  }
 
-path:nth-child(3){
+  path:nth-child(3){
     stroke-dasharray: 465;
     stroke-dashoffset: 465;
     animation: ${keyframeAnimation} 2s ease forwards 0.6s;
-}
+  }
 
-path:nth-child(4){
+  path:nth-child(4){
     stroke-dasharray: 353;
     stroke-dashoffset: 353;
     animation: ${keyframeAnimation} 2s ease forwards 0.9s;
-}
+  }
 
-path:nth-child(5){
+  path:nth-child(5){
     stroke-dasharray: 189;
     stroke-dashoffset: 189;
     animation: ${keyframeAnimation} 2s ease forwards 1;
-}
+  }
 
-path:nth-child(6){
+  path:nth-child(6){
     stroke-dasharray: 375;
     stroke-dashoffset: 375;
     animation: ${keyframeAnimation} 2s ease forwards 1.5s;
-}
+  }
 
-path:nth-child(7){
+  path:nth-child(7){
     stroke-dasharray: 189;
     stroke-dashoffset: 189;
     animation: ${keyframeAnimation} 2s ease forwards 1.8s;
-}
+  }
 
-path:nth-child(8){
+  path:nth-child(8){
     stroke-dasharray: 416;
     stroke-dashoffset: 416;
     animation: ${keyframeAnimation} 2s ease forwards 1;
-}
+  }
 
-path:nth-child(9){
+  path:nth-child(9){
     stroke-dasharray: 439;
     stroke-dashoffset: 439;
     animation: ${keyframeAnimation} 2s ease forwards 1;
-}
-`
-
-export const AnimatedLogoContainer = styled.img<AnimatedLogoProps>`
-display: flex;
-width: ${(props) => props.width};
-height: ${(props) => props.height};
-padding: ${(props) => props.padding};
-margin: ${(props) => props.margin};
+  }
 `;
--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -3,15 +3,13 @@ import * as S from "./HeaderStyled";
 import { iconScroll } from "../../../shared/img";
 import { AnimatedLogo } from "../../elements";
 
-const Header = () => {
-  return (
-    <S.HeaderContainer>
-      <AnimatedLogo width="260px" />
-      <S.ScrollLink href="#SectionSlide">
-        <S.ScrollImage src={iconScroll} alt="Clique para rolar" />
-      </S.ScrollLink>
-    </S.HeaderContainer>
-  );
-};
+const Header = () => (
+  <S.HeaderContainer>
+    <AnimatedLogo />
+    <S.ScrollLink href="#SectionSlide">
+      <S.ScrollImage src={iconScroll} alt="Clique para rolar" />
+    </S.ScrollLink>
+  </S.HeaderContainer>
+);
 
 export default Header;
