71d75652b56920f60825922f145536424c9ccbe9
Ramon <ramon1234rxm@gmail.com>
2022-02-16 11:30:26 -0300

    chore: remove unused files

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -19,16 +19,7 @@ module.exports = (plop) => {
         path: '../src/components/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/styles.ts.hbs'
       },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/stories.tsx.hbs'
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
-        templateFile: 'templates/test.tsx.hbs'
-      }
+
     ]
   })
 }
