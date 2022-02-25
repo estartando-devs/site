68ba053805d4b77186cbb15db0e2ec5bf4871f8c
Ramon <ramon1234rxm@gmail.com>
2022-02-25 17:59:22 -0300

    feat(header): implement header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,31 @@
-import Header from './Header';
+import Image from 'next/image';
+import { LinkWrapper } from '../LinkWrapper';
+import { Typography } from '../Typography';
+import * as S from './styles';
+
+const Header = () => (
+  <S.ContainerHeader>
+    <S.SectionHeader>
+      <S.ContainerImage>
+        <Image
+          src="/images/logo"
+          width={91}
+          height={132}
+          alt="Logo estartando devs"
+          title="Logo estartando devs."
+        />
+      </S.ContainerImage>
+      <S.ContainerHeading>
+        <Typography variant="h1">Estartando Devs</Typography>
+        <Typography weight="400">
+          Transformando o mundo através da tecnologia
+        </Typography>
+        <S.ContainerButton>
+          <LinkWrapper href="/">Faça parte!</LinkWrapper>
+        </S.ContainerButton>
+      </S.ContainerHeading>
+    </S.SectionHeader>
+  </S.ContainerHeader>
+);
 
 export { Header };
