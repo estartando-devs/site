2ce1db01f08c901b1749fe92dfb1e8b4135d90bd
jonataspinto <jonatastdb_jp@hotmail.com>
2022-02-25 11:44:45 -0300

    feat: create social buttons

--- src/components/Footer/SocialShareButtons/SocialShareButtons.tsx
+++ src/components/Footer/SocialShareButtons/SocialShareButtons.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { socialLinks } from './_mock';
+import { useSocialButtons } from './useSocialButtons';
 import * as S from './SocialShareButtonsStyled';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export const SocialShareButtons = () => (
-  <S.SocialShareContainer>
-    {socialLinks.map(({ title }) => (
-      <S.SocialShareContent key={title} title={title}>
-        {title}
-      </S.SocialShareContent>
-    ))}
-  </S.SocialShareContainer>
-);
+export const SocialShareButtons = () => {
+  const { socialLinks } = useSocialButtons();
+
+  return (
+    <S.SocialShareContainer>
+      {socialLinks.map(({ title, href, imageProps }) => (
+        <Link key={title} href={href}>
+          <a target="_blank">
+            <S.SocialShareContent title={title}>
+              <Image {...imageProps} />
+            </S.SocialShareContent>
+          </a>
+        </Link>
+      ))}
+    </S.SocialShareContainer>
+  );
+};
