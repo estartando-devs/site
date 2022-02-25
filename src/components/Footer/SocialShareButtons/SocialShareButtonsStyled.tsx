623ddf51de4114510e2df9d33e6f5936284ac233
jonataspinto <jonatastdb_jp@hotmail.com>
2022-02-25 00:54:15 -0300

    feat: initial commit

---
+++ src/components/Footer/SocialShareButtons/SocialShareButtonsStyled.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Card } from '../../Card';
+
+export const SocialShareContainer = styled.div`
+  display: flex;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SocialShareContent = styled(Card)`
+  padding: 8px;
+  justify-content: center;
+  align-items: center;
+`;
