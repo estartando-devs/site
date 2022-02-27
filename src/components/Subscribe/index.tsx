3f02e48c7a88d6ce0fea5ddb31966193dac539fa
Calenzo <calenzodevs@gmail.com>
2022-02-27 00:24:15 -0300

    feat: creating the component of 'Subscribe'

---
+++ src/components/Subscribe/index.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import { Typography } from '../Typography';
+
+import * as S from './styles';
+
+const Subscribe = () => (
+  <S.ContainerSubscribe>
+    <S.ContainerSubscribeImage>
+      <Image
+        src="/images/inscricao-check"
+        width={294}
+        height={364}
+        alt="Logo estartando devs"
+        title="Logo estartando devs."
+      />
+    </S.ContainerSubscribeImage>
+    <S.ContainerSubscribeContent>
+      <S.ContainerSubscribeContentText>
+        {`Inscreva-se!`}
+      </S.ContainerSubscribeContentText>
+      <S.ContainerSubscribeContentButton>
+        {`Faça parte!`}
+      </S.ContainerSubscribeContentButton>
+      <S.ContainerSubscribeContentDoubts>
+        <Typography variant="body3">{`Dúvidas? Veja as`}</Typography>
+        <S.ContainerSubscribeContentDoubtsLink>{`perguntas mais frequentes`}</S.ContainerSubscribeContentDoubtsLink>
+      </S.ContainerSubscribeContentDoubts>
+    </S.ContainerSubscribeContent>
+    <S.ContainerSubscribeImage>
+      <Image
+        src="/images/inscricao-calendar"
+        width={294}
+        height={364}
+        alt="Logo estartando devs"
+        title="Logo estartando devs."
+      />
+    </S.ContainerSubscribeImage>
+  </S.ContainerSubscribe>
+);
+
+export { Subscribe };
