4848e4e7f34d99f3a3eed8a0ced180ba5810119a
João Pedro <joaof082@gmail.com>
2022-03-01 22:22:30 -0300

    feat: mock for Description profile
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ src/_mocks/DescriptionProfile.mock.ts
@@ -0,0 +1,22 @@
+type DescriptionProfile = {
+  description: string;
+};
+
+export const descriptionProfileMock: DescriptionProfile[] = [
+  {
+    description: `Buscamos pessoas que gostem de desafios, sejam determinadas e
+    responsáveis. Nossa formação é totalmente <strong>gratuita.</strong> A única coisa que
+    cobramos é <strong>dedicação.</strong>`,
+  },
+  {
+    description: ` Se você tem de <strong> 18 a 29 anos </strong> e mora <strong> em qualquer lugar, 
+    </strong>  você pode se
+    inscrever!`,
+  },
+  {
+    description: `É legal que você saiba usar mouse e teclado e pesquisar na internet, e
+    tenha bastante disposição para estudar. Ah, é importante ter
+    disponibilidade nas terças e quintas, de 19:30 às 21:30, pois esse
+    será o horário das aulas.`,
+  },
+];
