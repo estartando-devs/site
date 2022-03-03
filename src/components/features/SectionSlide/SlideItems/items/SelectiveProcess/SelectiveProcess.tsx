0a88c9f630a43c0ccdfc1cac3f432aa4123207f4
jonataspinto <jonatastdb_jp@hotmail.com>
2022-03-03 13:15:41 -0300

    fix: remove redirect subscription button

--- src/components/features/SectionSlide/SlideItems/items/SelectiveProcess/SelectiveProcess.tsx
+++ src/components/features/SectionSlide/SlideItems/items/SelectiveProcess/SelectiveProcess.tsx
@@ -4,7 +4,7 @@ import { IItem } from "../../SlideItems";
 import { iconBack, slideBtn2, dotStepTitle1 } from "../../../assets";
 import { ButtonPrevious, ButtonNext } from "../../SlideItemsStyled";
 import { selectiveProcessMock } from "../../../../../../_mocks";
-import config from "../../../../../../config/constants";
+// import config from "../../../../../../config/constants";
 
 interface ISideStep {
   position: number;
@@ -12,7 +12,7 @@ interface ISideStep {
   description: string;
 }
 
-const SideStep = ({ position, title, description }: ISideStep) => {
+function SideStep({ position, title, description }: ISideStep) {
   const isRight = (position + 1) % 2 === 1 || false; // 1 even, 0 odd
 
   return (
@@ -32,43 +32,47 @@ const SideStep = ({ position, title, description }: ISideStep) => {
       </S.StepContent>
     </S.SideStepContainer>
   );
-};
+}
 
-const ButtonBox = () => (
-  <S.ButtonContainer>
-    <S.ButtonBox>
-      <S.ButtonLink target="blank" href={config.INSCRICOES_URL}>
-        <S.StepButton onClick={() => {}}>Faça Parte!</S.StepButton>
-      </S.ButtonLink>
-    </S.ButtonBox>
-  </S.ButtonContainer>
-);
+// function ButtonBox() {
+//   return (
+//     <S.ButtonContainer>
+//       <S.ButtonBox>
+//         <S.ButtonLink target="blank" href={config.INSCRICOES_URL}>
+//           <S.StepButton onClick={() => {}}>Faça Parte!</S.StepButton>
+//         </S.ButtonLink>
+//       </S.ButtonBox>
+//     </S.ButtonContainer>
+//   );
+// }
 
-const SelectiveProcess = ({ position, onNext, onPrevious }: IItem) => (
-  <S.SelectiveProcessContainer bgColor="#D3D3D3" position={position}>
-    <ButtonPrevious src={iconBack} onClick={onPrevious} alt="Voltar" />
-    <S.SelectiveProcessContent>
-      <S.SectionHeader>
-        <div>
-          <S.SelectiveProcessTitle>PROCESSO</S.SelectiveProcessTitle>
-          <S.SelectiveProcessTitle>
-            <b>SELETIVO</b>
-          </S.SelectiveProcessTitle>
-          <S.SelectiveProcessText>
-            Nosso processo seletivo é dividido em 3 etapas:
-          </S.SelectiveProcessText>
-        </div>
-      </S.SectionHeader>
-      <S.StepsContainer>
-        <S.VerticalLine />
-        {selectiveProcessMock.map((step, index) => (
-          <SideStep key={step.title} position={index} {...step} />
-        ))}
-      </S.StepsContainer>
-      <ButtonBox />
-    </S.SelectiveProcessContent>
-    <ButtonNext src={slideBtn2} alt="clique e conheça" onClick={onNext} />
-  </S.SelectiveProcessContainer>
-);
+function SelectiveProcess({ position, onNext, onPrevious }: IItem) {
+  return (
+    <S.SelectiveProcessContainer bgColor="#D3D3D3" position={position}>
+      <ButtonPrevious src={iconBack} onClick={onPrevious} alt="Voltar" />
+      <S.SelectiveProcessContent>
+        <S.SectionHeader>
+          <div>
+            <S.SelectiveProcessTitle>PROCESSO</S.SelectiveProcessTitle>
+            <S.SelectiveProcessTitle>
+              <b>SELETIVO</b>
+            </S.SelectiveProcessTitle>
+            <S.SelectiveProcessText>
+              Nosso processo seletivo é dividido em 3 etapas:
+            </S.SelectiveProcessText>
+          </div>
+        </S.SectionHeader>
+        <S.StepsContainer>
+          <S.VerticalLine />
+          {selectiveProcessMock.map((step, index) => (
+            <SideStep key={step.title} position={index} {...step} />
+          ))}
+        </S.StepsContainer>
+        {/* <ButtonBox /> */}
+      </S.SelectiveProcessContent>
+      <ButtonNext src={slideBtn2} alt="clique e conheça" onClick={onNext} />
+    </S.SelectiveProcessContainer>
+  );
+}
 
 export default SelectiveProcess;
