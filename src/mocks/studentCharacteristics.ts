b51aaa2994703d825fb764db4ee8f671e46dbe85
valuche <yago.pina_storm@prestador.globo>
2022-03-02 23:56:53 -0300

    chore: creating list that feed the cards

---
+++ src/mocks/studentCharacteristics.ts
@@ -0,0 +1,24 @@
+const characteristicsList = [
+  {
+    image: '/xadrez.svg',
+    textCard: 'Aluno como protagonista do processo de aprendizado',
+  },
+  {
+    image: '/emocoes.svg',
+    textCard: 'Transparência total nas relações',
+  },
+  {
+    image: '/alvo.svg',
+    textCard: 'Foco na qualidade do que é desenvolvido',
+  },
+  {
+    image: '/gatinho.svg',
+    textCard: 'Proximidade com os alunos',
+  },
+  {
+    image: '/boliche.svg',
+    textCard: 'Prática constante',
+  },
+];
+
+export { characteristicsList };
