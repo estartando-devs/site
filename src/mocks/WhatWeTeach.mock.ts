b72890e5be0ac952c8466405682ff675caf916d3
Ramon <ramon1234rxm@gmail.com>
2022-03-04 09:53:41 -0300

    refactor: add mocks and passing dangerous html

---
+++ src/mocks/WhatWeTeach.mock.ts
@@ -0,0 +1,14 @@
+export type WhatWeTeachProps = {
+  title: string,
+  description: string,
+}
+
+export const whatWeTeachMock: WhatWeTeachProps = {
+  title: `O que nós <span>ensinamos </span>?`,
+  description: `Acreditamos no poder de <span> transformação social </span>
+  através da Tecnologia da Informação. Por isso, oferecemos uma formação
+  objetiva e focada nas exigências do mercado de TI. Os três cursos
+  incluem ainda conhecimentos em métodos de <span> gestão ágil </span>
+  de projetos utilizados no mercado para o desenvolvimento de trabalhos em
+  equipe, além de assessoria no Linkedin e direcionamento de carreira.`,
+};
