3469487e5a00b7d31819b6d960dcdd6453e34371
valuche <yago.pina_storm@prestador.globo>
2022-03-04 11:03:12 -0300

    will it go

--- src/components/StudentCharacteristicsCard/styles.ts
+++ src/components/StudentCharacteristicsCard/styles.ts
@@ -6,14 +6,15 @@ const CardContainer = styled.section`
   min-width: 13rem;
   height: 6.2rem;
   box-shadow: 0.3rem 0.3rem 0.5rem -0.3rem rgb(0 0 0 / 98%),
-    -0.1rem -0.1rem 0.3rem -0.3rem white;
+    ${({ theme }) =>
+      `-0.1rem -0.1rem 0.3rem -0.3rem ${theme.palette.design.white}`};
   padding: 0.3rem 0.9rem;
   position: relative;
   border-radius: 0.3rem;
   display: flex;
   align-items: center;
   gap: 0.6rem;
-  background-color: #1e1e1e;
+  background-color: ${({ theme }) => theme.palette.gray[1]};
 `;
 
 const TextCharacteristics = styled.p`
@@ -23,18 +24,19 @@ const TextCharacteristics = styled.p`
 `;
 
 const Division = styled.div`
-  background-color: #7b7a7a;
+  background-color: ${({ theme }) => theme.palette.gray[3]};
   height: 60%;
   width: 0.06rem;
 `;
 const FormatContainerLeft = styled.div`
   width: 2.625rem;
   height: 0.812rem;
-  background-color: #1e1e1e;
+  background-color: ${({ theme }) => theme.palette.gray[1]};
   position: absolute;
   top: -0.275rem;
   left: 0;
-  box-shadow: -0.25rem -0.125rem 0.312rem -0.375rem white;
+  box-shadow: ${({ theme }) =>
+    `-0.25rem -0.125rem 0.312rem -0.375rem ${theme.palette.design.white}`};
   border-top-right-radius: 1.375rem;
   border-top-left-radius: 0.312rem;
   transform: rotateZ(16deg);
@@ -43,11 +45,12 @@ const FormatContainerLeft = styled.div`
 const FormatContainerRight = styled.div`
   width: 2.625rem;
   height: 0.81rem;
-  background-color: #1e1e1e;
+  background-color: ${({ theme }) => theme.palette.gray[1]};
   position: absolute;
   top: -0.3125rem;
   right: 0.125rem;
-  box-shadow: 0.1875rem -0.1875rem 0.1875rem -0.3125rem white;
+  box-shadow: ${({ theme }) =>
+    `0.1875rem -0.1875rem 0.1875rem -0.3125rem ${theme.palette.design.white}`};
   border-top-left-radius: 1.375rem;
   border-top-right-radius: 0.3125rem;
   transform: rotateZ(-17deg);
