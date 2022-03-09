6c19a3cbeb93662cb5f9bb7c97c27cd43a1749ff
Ramon <ramon1234rxm@gmail.com>
2022-03-09 16:06:31 -0300

    chore: remove unnecessary meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,17 +18,12 @@ function App({ Component, pageProps }: AppProps) {
           content="8kDtWUmUQEh7QXoj_shRaxcgYAVpHs_YQ7TeniN0kmI"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#81CAA8" />
         <meta name="apple-mobile-web-app-status-bar-style" content="#81CAA8" />
         <meta name="msapplication-navbutton-color" content="#81CAA8" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           property="og:image"
           content="https://media-exp1.licdn.com/dms/image/C4D0BAQEp4FVZvqZz2Q/company-logo_200_200/0?e=2159024400&v=beta&t=7u2-5asQNxjGYAgF18kGeHiIc-WXNIqwNscWXhuRzoY"
-        />
-        <meta
-          name="description"
-          content="Acreditamos no poder de transformação social através da tecnologia"
         />
       </Head>
       <GlobalStyles />
