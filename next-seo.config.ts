8b0a22d5da2257e6d60a14224637fd7082f94e75
Ramon <ramon1234rxm@gmail.com>
2022-03-10 11:36:40 -0300

    fix: add metatags and description content for seo

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,8 +1,13 @@
 export default {
+  description:
+    'Acreditamos no poder de transformação social através da tecnologia',
+  canonical: 'https://estartandodevs.com.br/',
   openGraph: {
     type: 'website',
     locale: 'pt_BR',
     url: 'https://estartandodevs.com.br',
     site_name: 'Estartando Devs',
+    description:
+      'Acreditamos no poder de transformação social através da tecnologia',
   },
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,7 @@ function App({ Component, pageProps }: AppProps) {
           name="google-site-verification"
           content="8kDtWUmUQEh7QXoj_shRaxcgYAVpHs_YQ7TeniN0kmI"
         />
+        <meta name="theme-color" content="#81CAA8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="apple-mobile-web-app-status-bar-style" content="#81CAA8" />
         <meta name="msapplication-navbutton-color" content="#81CAA8" />
