ed36cb19be0179c46294f43d70a3b14c93f5b972
Jarilson <jarilson.rk@gmail.com>
2022-03-18 10:43:15 -0300

    Reafact: Adjusting animating.

--- src/components/SelectiveProcess/index.tsx
+++ src/components/SelectiveProcess/index.tsx
@@ -20,7 +20,12 @@ export const SelectiveProcess = ({
     <S.Title variant="h2" dangerouslySetInnerHTML={{ __html: title }} />
 
     {stagesList.map(({ key, stage }) => (
-      <S.ContentStages key={key}>
+      <S.ContentStages
+        key={key}
+        data-aos="fade-up"
+        data-aos-easing="ease-in-out"
+        data-aos-anchor-placement="top-center"
+      >
         <S.StageTitle variant="h3">{stage.title}</S.StageTitle>
         <S.Text variant="body2">{stage.description}</S.Text>
       </S.ContentStages>
