34db29f813723b4539112f232066e5397ca43b82
jonataspinto <jonatastdb_jp@hotmail.com>
2022-04-04 16:26:27 -0300

    feat(header, subscription): removing call to actions of subscribe

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,8 @@
 import { Logo } from '../Logo';
-import { LinkWrapper } from '../LinkWrapper';
 import { Typography } from '../Typography';
 import * as S from './styles';
 
-const Header = () => (
+export const Header = () => (
   <S.ContainerHeader>
     <S.SectionHeader>
       <S.ContainerImage>
@@ -14,16 +13,7 @@ const Header = () => (
         <Typography weight="400">
           Transformando o mundo através da tecnologia
         </Typography>
-        <S.ContainerButton>
-          <LinkWrapper href={process.env.NEXT_PUBLIC_INSCRICOES_URL || ''}>
-            <a target="_blank" rel="noreferrer noopener">
-              Faça parte!
-            </a>
-          </LinkWrapper>
-        </S.ContainerButton>
       </S.ContainerHeading>
     </S.SectionHeader>
   </S.ContainerHeader>
 );
-
-export { Header };
