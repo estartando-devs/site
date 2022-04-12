3bc68e82d8274d7681ea66613b98756ce7666ff2
Ramon <ramon1234rxm@gmail.com>
2022-04-12 11:18:36 -0300

    feat: stylize and improve card form

--- src/components/IdCardForm/index.tsx
+++ src/components/IdCardForm/index.tsx
@@ -1,13 +1,29 @@
+import {
+  Box,
+  Button,
+  FormErrorMessage,
+  FormControl,
+  Heading,
+  Input,
+} from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useTheme } from 'styled-components';
 import * as S from './styles';
 
 export const IdCardForm = () => {
-  const { register, handleSubmit: handleSubimtUseForm } = useForm();
+  const {
+    register,
+    handleSubmit: onSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const [imagePreview, setImagePreview] = useState<Blob | MediaSource>();
   const router = useRouter();
-  const handleSubmit = ({ name, email }: Record<string, string>) => {
-    router.push({ pathname: '/id-card/[email]', query: { name, email } });
+  const theme = useTheme();
+
+  const handleSubmit = ({ email }: Record<string, string>) => {
+    router.push({ pathname: '/id-card/[email]', query: { email } });
   };
 
   const handleUpload = (event: ChangeEvent<HTMLInputElement>) => {
@@ -28,24 +44,88 @@ export const IdCardForm = () => {
       event.target.files[0] &&
       event.target.files[0].size > 80000
     ) {
-      return alert('arquivo muito grande');
+      return alert('O arquivo é muito grande');
     }
     if (event.target.files && event.target.files[0]) {
       reader.readAsDataURL(event.target.files[0]);
+      setImagePreview(event.target.files[0]);
     }
   };
+
   return (
     <S.Container>
-      <h1>
-        Crie seu ID Card personalizado e divulge nas suas redes. Não esqueça de
-        nos marcar.
-      </h1>
-      <form onSubmit={handleSubimtUseForm(handleSubmit)}>
-        <input type="text" {...register('name')} />
-        <input type="text" {...register('email')} />
-        <input type="file" max-file-size="1024" onChange={handleUpload} />
-        <button type="submit">Criar</button>
+      <Heading color="white">
+        Crie seu ID Card personalizado e divulge nas suas redes.
+        <br /> Não esqueça de nos marcar.
+      </Heading>
+      <form onSubmit={onSubmit(handleSubmit)}>
+        <FormControl isInvalid={errors.email}>
+          <Input
+            placeholder="E-mail"
+            type="text"
+            id="email"
+            {...register('email', {
+              required: 'E-mail é obrigatório',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Insira um e-mail válido',
+              },
+            })}
+            textColor="white"
+          />
+          <FormErrorMessage>
+            {errors.email && errors.email.message}
+          </FormErrorMessage>
+        </FormControl>
+        <S.FileInput
+          type="file"
+          max-file-size="1024"
+          onChange={handleUpload}
+          id="file-input"
+        />
+        <S.FileInputLabel htmlFor="file-input">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="upload"
+            className="svg-inline--fa fa-upload fa-w-16"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="currentColor"
+              d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+            ></path>
+          </svg>
+          <span>Carregar foto</span>
+        </S.FileInputLabel>
+        <Box margin="0 auto">
+          {imagePreview && (
+            <S.ImagePreview
+              layout="fixed"
+              src={URL.createObjectURL(imagePreview)}
+              alt="Preview"
+              width="100%"
+              height="100%"
+              objectFit="contain"
+            />
+          )}
+        </Box>
+        <Button
+          type="submit"
+          color={theme.palette.design.white}
+          backgroundColor={theme.palette.primary.main}
+          isLoading={isSubmitting}
+          _hover={{
+            backgroundColor: theme.palette.design.green_dark,
+          }}
+        >
+          Criar
+        </Button>
       </form>
+      <S.ImagePreviewContainer />
     </S.Container>
   );
 };
--- src/components/IdCardForm/styles.ts
+++ src/components/IdCardForm/styles.ts
@@ -1,6 +1,8 @@
+import Image from 'next/image';
 import styled from 'styled-components';
 
 export const Container = styled.main`
+  padding-top: 2rem;
   height: 100vh;
   display: flex;
   flex-direction: column;
@@ -10,6 +12,59 @@ export const Container = styled.main`
     display: flex;
     flex-direction: column;
     gap: 1rem;
-    padding: 0 15rem;
+    padding: 0 30rem;
   }
+
+  h2 {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+`;
+
+export const ImagePreview = styled(Image)`
+  border-radius: 50%;
+`;
+
+export const FileInput = styled.input`
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
+`;
+
+export const FileInputLabel = styled.label`
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.25rem;
+  font-size: 0.875rem;
+  font-weight: 600;
+  color: #fff;
+  font-size: 0.875rem;
+  padding: 0.625rem 0.75rem;
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  box-shadow: 0 0 0.125rem rgba(0, 0, 0, 0.25);
+  transition: background-color 0.2s ease-in-out;
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.design.green_dark};
+  }
+
+  svg {
+    height: 1rem;
+    margin-right: 0.25rem;
+  }
+`;
+
+export const ImagePreviewContainer = styled.div`
+  background: url('/EDcard.jpeg') no-repeat;
+  padding: 0 2rem;
+  background-size: contain;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+  max-width: 30rem;
+  max-height: 30rem;
 `;
