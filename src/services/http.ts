5edee4eb38e3f2c378f671060bd845f9392b7a6f
jonataspinto <jonatastdb_jp@hotmail.com>
2022-04-12 00:54:03 -0300

    feat: create services

---
+++ src/services/http.ts
@@ -0,0 +1,12 @@
+export interface HttpResponse<T> extends Response {
+  parsedBody?: T;
+}
+
+export async function http<T>(
+  request: RequestInfo,
+  config?: RequestInit
+): Promise<HttpResponse<T>> {
+  const response: HttpResponse<T> = await fetch(request, config);
+  response.parsedBody = await response.json();
+  return response;
+}
