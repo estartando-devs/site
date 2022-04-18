3ab9400892061d0521f2e3f2fe1668789dae2ce8
Ramon <ramon1234rxm@gmail.com>
2022-04-18 09:54:48 -0300

    chore: add jsdoc

--- src/utils/cropImage.ts
+++ src/utils/cropImage.ts
@@ -5,6 +5,10 @@ export type CroppedArea = {
   y: number;
 };
 
+/**
+ * @description: Create a cropped image
+ * @param {string} url: url image to be cropped
+ */
 const createImage = (
   url: string | ArrayBuffer | undefined
 ): Promise<CanvasImageSource> =>
@@ -16,15 +20,27 @@ const createImage = (
     image.src = url as string;
   });
 
-function getRadianAngle(degreeValue: number) {
+/**
+ * @description: Capture the radian of a given angle
+ * @param {number} degreeValue: degree of rotation
+ * @returns {number} radian angle
+ */
+function getRadianAngle(degreeValue: number): number {
   return (degreeValue * Math.PI) / 180;
 }
 
+/**
+ * @description: Crop an image
+ * @param {string | ArrayBuffer | undefined} imageSrc: image to be cropped
+ * @param {CroppedArea} pixelCrop: area to be cropped
+ * @params {number} [rotation=0]: degree of rotation
+ * @returns {Promise<CanvasImageSource>} canvas image
+ */
 export async function getCroppedImg(
   imageSrc: string | ArrayBuffer | undefined,
   pixelCrop: CroppedArea,
   rotation = 0
-) {
+): Promise<HTMLCanvasElement> {
   const image = await createImage(imageSrc);
   const canvas = document.createElement('canvas');
   const canvasCtx = canvas.getContext('2d');
@@ -32,17 +48,13 @@ export async function getCroppedImg(
   const maxSize = Math.max(image.width as number, image.height as number);
   const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2));
 
-  // set each dimensions to double largest dimension to allow for a safe area for the
-  // image to rotate in without being clipped by canvas context
   canvas.width = safeArea;
   canvas.height = safeArea;
 
-  // translate canvas context to a central location on image to allow rotating around the center.
   canvasCtx?.translate(safeArea / 2, safeArea / 2);
   canvasCtx?.rotate(getRadianAngle(rotation));
   canvasCtx?.translate(-safeArea / 2, -safeArea / 2);
 
-  // draw rotated image and store data.
   canvasCtx?.drawImage(
     image,
     safeArea / 2 - Number(image.width) * 0.5,
@@ -51,18 +63,14 @@ export async function getCroppedImg(
 
   const data = canvasCtx?.getImageData(0, 0, safeArea, safeArea);
 
-  // set canvas width to final desired crop size - this will clear existing context
   canvas.width = pixelCrop.width;
   canvas.height = pixelCrop.height;
 
-  // paste generated rotate image with correct offsets for x,y crop values.
   canvasCtx?.putImageData(
     data as ImageData,
     0 - safeArea / 2 + Number(image.width) * 0.5 - pixelCrop.x,
     0 - safeArea / 2 + Number(image.height) * 0.5 - pixelCrop.y
   );
 
-  // As Base64 string
-  // return canvas.toDataURL("image/jpeg");
   return canvas;
 }
