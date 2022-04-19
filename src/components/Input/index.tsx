3deb63a4a7fc66ce962c00afe0e38c9617feaf28
Ramon <ramon1234rxm@gmail.com>
2022-04-18 21:51:58 -0300

    feat: make a input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,28 @@
+import { useFormContext } from 'react-hook-form';
+import * as S from './styles';
+import { InputProps } from './types/InputProps';
+
+export const Input = ({
+  id,
+  validation,
+  placeholder,
+  ...restProps
+}: InputProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const isError = errors && errors[id as string]?.message;
+
+  return (
+    <S.ContainerField>
+      <S.Input
+        isError={!!isError}
+        {...register(id, validation)}
+        {...restProps}
+      />
+      <S.Label htmlFor={id}>{placeholder}</S.Label>
+      {isError && <S.Error>{errors[id as string]?.message}</S.Error>}
+    </S.ContainerField>
+  );
+};
