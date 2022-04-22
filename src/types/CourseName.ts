b70ddc2a6f219eadd8abe784b3dc1a8e7b808cef
jonataspinto <jonatastdb_jp@hotmail.com>
2022-04-22 03:54:47 -0300

    feat: create course type

---
+++ src/types/CourseName.ts
@@ -0,0 +1,10 @@
+export enum CourseNameByKey {
+  'frontend' = 'Desenvolvimento Web',
+  'backend' = 'Desenvolvimento Backend',
+  'design' = 'Design UI/UX',
+}
+export enum CourseKeyByName {
+  'Desenvolvimento Web' = 'frontend',
+  'Desenvolvimento Backend' = 'backend',
+  'Design UI/UX' = 'design',
+}
