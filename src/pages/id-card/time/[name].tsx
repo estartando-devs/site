271984ea8c75a140eaf9cd63a4178fe427380038
jonataspinto <jonatastdb_jp@hotmail.com>
2022-04-22 03:59:41 -0300

    feature: create page to generate team id card

---
+++ src/pages/id-card/time/[name].tsx
@@ -0,0 +1,96 @@
+import { Box, useToast } from '@chakra-ui/react';
+import { getCookie } from 'cookies-next';
+import { GetServerSideProps } from 'next';
+import { NextSeo } from 'next-seo';
+import { Layout } from '../../../components';
+import {
+  IdCard as IdCardComponent,
+  IdCardProps,
+} from '../../../components/IdCard';
+import { NotFoundIdCard } from '../../../components/NotFoundIdCard';
+import { cleanZipcode, getAddressByCep } from '../../../services';
+import { CourseNameByKey } from '../../../types';
+
+export const getServerSideProps: GetServerSideProps = async ({
+  query,
+  req,
+  res,
+}) => {
+  const nagatoCookie = getCookie('nagato', { req, res }) || '{}';
+  const { zipcode, course } = JSON.parse(nagatoCookie as string);
+  const { localidade = null, uf = null } = await getAddressByCep(
+    cleanZipcode(zipcode)
+  );
+
+  const profile = {
+    name: query?.name,
+    image: {
+      src: 'https://res.cloudinary.com/elite-devs/images/logo',
+      alt: `imagem monitor(a) ${query?.name}`,
+    },
+    course: course || CourseNameByKey.frontend,
+    address: {
+      city: localidade,
+      state: uf,
+    },
+  };
+  return {
+    props: {
+      profile,
+    },
+  };
+};
+
+export default function MyIdCard({
+  profile,
+  error,
+}: {
+  profile: IdCardProps;
+  error?: string;
+}) {
+  const toast = useToast();
+
+  if (error) {
+    return (
+      <Layout>
+        <NotFoundIdCard />;
+      </Layout>
+    );
+  }
+
+  toast({
+    position: 'top',
+    title: 'Tudo pronto!',
+    description:
+      'Agora você pode compartilhar seu id card. Não esqueça de nos marcar.',
+    status: 'success',
+    duration: 5000,
+    isClosable: true,
+  });
+
+  return (
+    <Layout>
+      <NextSeo
+        title="Estartando Devs | Id Card"
+        description="Crie seu ID Card personalizado e divulge nas suas redes. Não esqueça de nos marcar."
+        openGraph={{
+          type: 'website',
+          locale: 'pt_BR',
+          url: 'https://estartandodevs.com.br',
+          site_name: 'Estartando Devs',
+          title: 'Estartando Devs',
+        }}
+      />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+        padding="2rem"
+      >
+        <IdCardComponent {...profile} />
+      </Box>
+    </Layout>
+  );
+}
