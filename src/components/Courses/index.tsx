e1644b74530c4159055a0f1ec171dfde28865988
Ramon <ramon1234rxm@gmail.com>
2022-05-24 15:59:59 -0300

    refactor: remove Object.keys() and use Object.entries()

--- src/components/Courses/index.tsx
+++ src/components/Courses/index.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import { CourseData, CourseKey } from '../../mocks';
+import { CourseData } from '../../mocks';
 import { Course } from './Course';
 
 export const Courses = ({ coursesData }: { coursesData: CourseData }) => (
   <>
-    {Object.keys(coursesData).map((courseKey: string) => (
-      <Course
-        key={courseKey}
-        courseData={coursesData[courseKey as CourseKey]}
-      />
+    {Object.entries(coursesData).map(([key, value]) => (
+      <Course key={key} courseData={value} />
     ))}
   </>
 );
