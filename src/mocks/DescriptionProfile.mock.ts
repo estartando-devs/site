a9110e9423ebef1d10506fabeae70fbee91fa122
Ramon Xavier <ramon1234rxm@gmail.com>
2025-07-09 21:32:42 -0300

    refactor: Simplify student profile description in mock data
    - Revised the student profile description in DescriptionProfile.mock.ts to focus on key attributes of candidates, removing unnecessary details for clarity and conciseness.
    - Updated the format to enhance readability while maintaining essential information about the target audience.

--- src/mocks/DescriptionProfile.mock.ts
+++ src/mocks/DescriptionProfile.mock.ts
@@ -5,24 +5,12 @@ export type studentSectionProfile = {
 
 export const studentProfileSection: studentSectionProfile = {
   title: ` Qual o perfil de aluno que <br /> <strong>buscamos?</strong>`,
-  description: `Buscamos pessoas que gostem de desafios, sejam determinadas e
-  responsáveis. Nossa formação é totalmente <strong>gratuita.</strong> A
-  única coisa que cobramos é <strong>dedicação.</strong>
-  <br />
-  <br />
-  Para se inscrever você precisa: <br />
-  <br />
+  description: `
   <ul>
     <li>Pessoas em situação de <strong>vulnerabilidade social e econômica</strong>. A seleção priorizará candidatos de baixa renda, de modo a gerar oportunidades para quem enfrenta maiores barreiras no acesso à educação e ao mercado de trabalho;</li>
     <li>Pessoas <strong>dedicadas</strong> e que gostem de desafios;</li>
     <li>Pessoas <strong>responsáveis</strong> e <strong>determinadas</strong>;</li>
     <li>Pessoas de qualquer lugar do mundo com acesso à <strong>internet</strong>;</li>
     <li>Interessados em ingressar no mercado de trabalho na <strong>área de tecnologia</strong>.</li>
-  </ul>
-
-  <br />
-  <br />É legal que você saiba usar mouse e teclado, pesquisar na internet
-  e ter bastante disposição para estudar. Ah, é importante ter
-  disponibilidade nas terças e quintas, de 19:30 às 21:30, pois esse será
-  o horário das aulas.`,
+  </ul>`,
 };
